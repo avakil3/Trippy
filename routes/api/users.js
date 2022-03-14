59de5a73c546b187eb430588d44b2ea723809428
Shawn-Fries <samfoley275@gmail.com>
2022-03-14 18:51:19 -0400

    signup with username

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,11 +11,7 @@ const validateLoginInput = require('../../validation/login');
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.json({
-        id: req.user.id,
-        handle: req.user.handle,
-        email: req.user.email
-    });
+    res.json({ msg: 'Success' });
 })
 
 router.post('/register', (req, res) => {
@@ -25,17 +21,28 @@ router.post('/register', (req, res) => {
         return res.status(400).json(errors);
     }
 
+    // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                // Use the validations to send the error
-                errors.email = 'Email already exists';
-                return res.status(400).json(errors);
+                // Throw a 400 error if the email address already exists
+                return res.status(400).json({ email: "A user has already registered with this address" })
             } else {
+                // Otherwise create a new user
                 const newUser = new User({
-                    name: req.body.name,
+                    username: req.body.username,
                     email: req.body.email,
                     password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
                 })
             }
         })
@@ -48,6 +55,7 @@ router.post('/login', (req, res) => {
         return res.status(400).json(errors);
     }
 
+    const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
 
