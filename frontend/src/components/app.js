0183e4f704a4d0c3e84faed0d2b036f839974058
Aagam Vakil <aagamvakil@hotmail.com>
2022-03-14 11:47:51 -0400

    completed splash page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,6 +3,8 @@ import React from 'react';
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './main_page/main_page';
+import SplashPage from './splash_page/splash_page';
+import '../index.scss'
 // import LoginFormContainer from './session/login_form_container';
 // import SignupFormContainer from './session/signup_form_container';
 
@@ -10,7 +12,7 @@ import MainPage from './main_page/main_page';
 const App = () => (
   <div>
     <Routes>
-      <Route exact path="/" element={<MainPage/>} />
+      <Route exact path="/" element={<SplashPage/>} />
       {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
     </Routes>
---
+++ frontend/src/components/splash_page/splash_page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import trippyLogo from '../../../public/images/trippy-logo.jpeg';
+
+const SplashPage = () => {
+
+    return (
+          <div className="splash-container">
+            <div className="splash-header">
+              <div className="splash-logo-container">
+                <Link to="/" className="header-link">
+                  <img id="logo" src='images/trippy-logo.jpeg' alt="logo" />
+                </Link>
+              </div>
+              <ul className="splash-nav-bar">
+                <li className="links"><a className="links" href='https://github.com/avakil3/trippy' target="_blank" >GitHub</a></li>
+                <li className="hidden">│</li>
+                <li>
+                  <Link to={ "/signup" } className="links">Sign Up</Link>
+                </li>
+                <li><Link to={ "/login" } className="links">Log In</Link></li>
+              </ul>
+            </div>
+            <div className="main-splash-text">
+              {/* <img className='center-logo' src="images/trippy-logos-white.png" alt="" /> */}
+              {/* <p id="content-listening-is">A world to see.</p> */}
+              <p id="content-world">A world to see.</p>
+              <p id="splash-centered_content">Millions of destinations and experiences. Plan your next trip today.</p>
+              <Link to={ "/signup" } className="splash-btn">
+                  Sign up for Trippy
+              </Link>
+            </div>
+ 
+          </div>
+      )
+
+    }
+    
+    export default SplashPage;
