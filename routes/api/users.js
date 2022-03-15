deb763e1e96812317011acda81db85950be6b1d6
Shawn-Fries <samfoley275@gmail.com>
2022-03-15 08:45:15 -0400

    user forms change

--- routes/api/users.js
+++ routes/api/users.js
@@ -41,7 +41,6 @@ router.post('/register', (req, res) => {
                 })
 
                 bcrypt.genSalt(10, (err, salt) => {
-                    debugger;
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err;
                         newUser.password = hash;
