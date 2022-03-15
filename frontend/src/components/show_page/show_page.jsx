470f7ac3e13db73cd5663b33d33419024b208663
yakzaetz <yakzaetz@gmail.com>
2022-03-14 20:45:19 -0400

    add left arrow icon

--- frontend/src/components/show_page/show_page.jsx
+++ frontend/src/components/show_page/show_page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HeaderContainer from '../header/header_container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 class ShowPage extends React.Component{
 
@@ -9,11 +11,17 @@ class ShowPage extends React.Component{
             <div className='whole-page'>
                 <HeaderContainer/>
                 <div className='everything-but-the-header'>
+                    <Link to={"/mainpage"}><div id='left-arrow'><FontAwesomeIcon icon={faArrowLeft} /></div></Link>
                     <div className='show-container-item'>
                         <div className='left-side-of-show-container'>
                             <img src="images/showpage_placeholder.png" />
                         </div>
-                        <div className='right-side-of-show-container'></div>
+                        <div className='right-side-of-show-container'>
+                            <div className='header-of-right-side-of-show'></div>
+                            <div></div><h1>Title here</h1>
+                            <p>Region:</p>
+                            <p>Vacation type:</p>
+                        </div>
                     </div>
                 </div>
             </div>
