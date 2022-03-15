95e64368acdafc2f1adaef0073a06950a36e856c
yakzaetz <yakzaetz@gmail.com>
2022-03-14 21:01:15 -0400

    add keys.js

--- frontend/src/components/show_page/show_page.jsx
+++ frontend/src/components/show_page/show_page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeaderContainer from '../header/header_container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
 
 class ShowPage extends React.Component{
 
@@ -11,7 +12,7 @@ class ShowPage extends React.Component{
             <div className='whole-page'>
                 <HeaderContainer/>
                 <div className='everything-but-the-header'>
-                    <Link to={"/mainpage"}><div id='left-arrow'><FontAwesomeIcon icon={faArrowLeft} /></div></Link>
+                    <Link to={"/home"}><div id='left-arrow'><FontAwesomeIcon icon={faArrowLeft} /></div></Link>
                     <div className='show-container-item'>
                         <div className='left-side-of-show-container'>
                             <img src="images/showpage_placeholder.png" />
