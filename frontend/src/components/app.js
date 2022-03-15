29e32c07b256f08c90ebae8278e41cc15db99f2c
Aagam Vakil <aagamvakil@hotmail.com>
2022-03-15 09:26:33 -0400

    working on home page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,17 +3,17 @@ import React from 'react';
 // import { AuthRoute, ProtectedRoute, PrivateRoute } from '../util/route_util';
 import { PrivateRoute } from '../util/route_util';
 import { Routes, Route } from 'react-router-dom';
-import MainPage from './main_page/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import SplashPage from './splash_page/splash_page'
+import MainPageContainer from './main_page/main_page_container';
 import '../index.scss'
 
 const App = () => (
   <div>
     <Routes>
       <Route path="/" element={<SplashPage/>} />
-      <Route path="/home" element={<MainPage/>} />
+      <Route path="/home" element={<MainPageContainer/>} />
       <Route path="/signup" element={ <PrivateRoute>
                                         <SignupFormContainer /> 
                                       </PrivateRoute>
