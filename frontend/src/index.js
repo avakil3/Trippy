ceb4c95ddd1f4680d0c33f688e794f7c495b2bed
Shawn-Fries <samfoley275@gmail.com>
2022-03-16 09:29:03 -0400

    bugfix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 
 import { setAuthToken } from './util/session_api_util';
-// import { logout } from './actions/session_actions';
+import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentTime = Date.now() / 1000;
 
     if (decodedUser.exp < currentTime) {
-      // store.dispatch(logout());
+      store.dispatch(logout());
       window.location.href = '/login';
     }
   } else {
