d57d75c8002477bb7ab29f5022fb350f02d1e331
Shawn-Fries <samfoley275@gmail.com>
2022-03-16 11:39:03 -0400

    add demo user

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -20,7 +20,7 @@ class SignupForm extends React.Component {
     componentWillReceiveProps(nextProps) {
         if (nextProps.signedIn === true) {
             //console.log(this.props)
-            this.props.router.navigate("/login")
+            this.props.router.navigate("home")
         }
 
         this.setState({ errors: nextProps.errors })
