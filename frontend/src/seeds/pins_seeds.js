4624e570470eb988ad0dd76a64fba4cd7db9b756
Shawn-Fries <samfoley275@gmail.com>
2022-03-16 18:53:58 -0400

    add seed

--- frontend/src/seeds/pins_seeds.js
+++ frontend/src/seeds/pins_seeds.js
@@ -20,14 +20,34 @@ const pins = [
     // }),
 
     new Pin({
-        title: "",
-        tripType:[],
-        region: "",
-        location: "",
-        imageURL: "",
-        description: "",
-        extLink: ""
-        }),
+        title: "Camel Ride with Sunset in Desert Agafay",
+        tripType:['camping', 'adventure'],
+        region: "africa",
+        location: "Marrakech, Morocco",
+        imageURL: "https://trippy-seeds.s3.us-east-2.amazonaws.com/camel_ride.webp",
+        description: "Experience the sunset with a camel ride in the atmosphere of Agafay Desert",
+        extLink: "https://www.airbnb.com/experiences/2728896?currentTab=experience_tab&federatedSearchId=863a62fa-c3e7-4a7f-831b-48248885fa99&searchId=&sectionId=787e4044-f310-4db6-9b55-de08a7c823a0&source=p2"
+         }),
+
+    // new Pin({
+    //     title: "",
+    //     tripType: [],
+    //     region: "",
+    //     location: "",
+    //     imageURL: "",
+    //     description: "",
+    //     extLink: ""
+    // }),
+
+    // new Pin({
+    //     title: "",
+    //     tripType: [],
+    //     region: "",
+    //     location: "",
+    //     imageURL: "",
+    //     description: "",
+    //     extLink: ""
+    // }),
 
 ];
 
