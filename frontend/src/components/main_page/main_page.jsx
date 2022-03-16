a9b1f6d010ae1629d39d83b3ad84e072c77677d0
Shawn-Fries <samfoley275@gmail.com>
2022-03-16 18:55:41 -0400

    add seed

--- frontend/src/components/main_page/main_page.jsx
+++ frontend/src/components/main_page/main_page.jsx
@@ -1,114 +1,115 @@
 import React from 'react';
 import HeaderContainer from '../header/header_container';
-import {PinsFilter} from '../../util/selectors';
+import { PinsFilter } from '../../util/selectors';
 import Pin from '../pin_item/pin';
 
 class MainPage extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {filteredPins: [],
-    anywhere: true, //refers to region filter
-    northAmerica: false,
-    asia: false,
-    europe: false,
-    africa: false,
-    any: true, //refers to tripType filter
-    relaxation:false,
-    adventure: false,
-    camping: false,
-    roadTrip: false
+    this.state = {
+      filteredPins: [],
+      anywhere: true, //refers to region filter
+      northAmerica: false,
+      asia: false,
+      europe: false,
+      africa: false,
+      any: true, //refers to tripType filter
+      relaxation: false,
+      adventure: false,
+      camping: false,
+      roadTrip: false
 
-  };
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchPins()
-      .then(()=> this.setState({filteredPins: Object.values(this.props.pins)}));
+      .then(() => this.setState({ filteredPins: Object.values(this.props.pins) }));
 
     this.props.fetchLikes(this.props.currentUser);
   }
 
 
-  handleChange(e){
+  handleChange(e) {
     // debugger
-      this.setState({[e.target.value]: e.target.checked},()=> {
-        this.setState({filteredPins: PinsFilter(this.props.pins,this.state)});
-      })
-    }
+    this.setState({ [e.target.value]: e.target.checked }, () => {
+      this.setState({ filteredPins: PinsFilter(this.props.pins, this.state) });
+    })
+  }
 
-    deleteTest(){
-      this.props.deleteLike(Object.values(this.props.likes)[0])
-    }
+  deleteTest() {
+    this.props.deleteLike(Object.values(this.props.likes)[0])
+  }
 
   render() {
     return (
       <div className='body'>
-        <HeaderContainer/>
+        <HeaderContainer />
         <div className='filters-section'>
           <h1>Discover Unique Destinations and Experiences</h1>
           <div className='filters'>
             <div className='region'>
               <label>Where do you want to travel?</label>
 
-            <div className='checkboxes'>
-            <label >
-              <input type="checkbox" defaultChecked="true" name="region" value="anywhere" id='anywhere' onChange={this.handleChange}/>
-              Anywhere
-            </label>
+              <div className='checkboxes'>
+                <label >
+                  <input type="checkbox" defaultChecked="true" name="region" value="anywhere" id='anywhere' onChange={this.handleChange} />
+                  Anywhere
+                </label>
 
-            <label >
-              <input type="checkbox" name="region" value="northAmerica" id='northAmerica' onChange={this.handleChange}/>
-              North America
-            </label>
+                <label >
+                  <input type="checkbox" name="region" value="northAmerica" id='northAmerica' onChange={this.handleChange} />
+                  North America
+                </label>
 
-            <label >
-              <input type="checkbox" name="region" value="asia" id='asia' onChange={this.handleChange}/>
-              Asia
-            </label>
+                <label >
+                  <input type="checkbox" name="region" value="asia" id='asia' onChange={this.handleChange} />
+                  Asia
+                </label>
 
-            <label >
-              <input type="checkbox" name="region" value="europe" id='europe' onChange={this.handleChange}/>
-              Europe
-            </label>
+                <label >
+                  <input type="checkbox" name="region" value="europe" id='europe' onChange={this.handleChange} />
+                  Europe
+                </label>
 
-            <label >
-              <input type="checkbox" name="region" value="africa" id='africa' onChange={this.handleChange}/>
-              Africa
-            </label>
-            
-            </div>
+                <label >
+                  <input type="checkbox" name="region" value="africa" id='africa' onChange={this.handleChange} />
+                  Africa
+                </label>
+
+              </div>
             </div>
             <div className="trip-type">
-            <label>What type of trip?</label>
-           
-            <div className='checkboxes'>
-            <label >
-              <input type="checkbox" defaultChecked="true" name="tripType" value="any" id='any' onChange={this.handleChange}/>
-              Any
-            </label>
+              <label>What type of trip?</label>
 
-            <label >
-              <input type="checkbox" name="tripType" value="relaxation" id='relaxation' onChange={this.handleChange}/>
-              Relaxation
-            </label>
+              <div className='checkboxes'>
+                <label >
+                  <input type="checkbox" defaultChecked="true" name="tripType" value="any" id='any' onChange={this.handleChange} />
+                  Any
+                </label>
 
-            <label >
-              <input type="checkbox" name="tripType" value="adventure" id='adventure' onChange={this.handleChange}/>
-              Adventure
-            </label>
+                <label >
+                  <input type="checkbox" name="tripType" value="relaxation" id='relaxation' onChange={this.handleChange} />
+                  Relaxation
+                </label>
 
-            <label >
-              <input type="checkbox" name="tripType" value="camping" id='camping' onChange={this.handleChange}/>
-              Camping
-            </label>
+                <label >
+                  <input type="checkbox" name="tripType" value="adventure" id='adventure' onChange={this.handleChange} />
+                  Adventure
+                </label>
 
-            <label >
-              <input type="checkbox" name="tripType" value="roadTrip" id='roadTrip' onChange={this.handleChange}/>
-              Road Trip
-            </label>
-            </div>
+                <label >
+                  <input type="checkbox" name="tripType" value="camping" id='camping' onChange={this.handleChange} />
+                  Camping
+                </label>
+
+                <label >
+                  <input type="checkbox" name="tripType" value="roadTrip" id='roadTrip' onChange={this.handleChange} />
+                  Road Trip
+                </label>
+              </div>
             </div>
           </div>
         </div>
@@ -117,7 +118,7 @@ class MainPage extends React.Component {
           <h2>Destinations and experiences trending across your selections</h2>
           <hr />
           <div className="image-grid">
-              {this.state.filteredPins.map((pin)=> <Pin pin={pin} imgDimensions={imgDimensions()} key={pin._id} /> )}
+            {this.state.filteredPins.map((pin) => <Pin pin={pin} imgDimensions={imgDimensions()} key={pin._id} />)}
           </div>
         </div>
       </div>
@@ -127,10 +128,10 @@ class MainPage extends React.Component {
 
 export default MainPage;
 
-function imgDimensions(){
+function imgDimensions() {
   // const widthArr = [1,2,3];
-  const heightArr = [1,2,3];
+  const heightArr = [1, 2, 3];
   // const randomColWidth = widthArr[Math.floor(Math.random()* widthArr.length)];
-  const randomColHeight = heightArr[Math.floor(Math.random()* heightArr.length)];
+  const randomColHeight = heightArr[Math.floor(Math.random() * heightArr.length)];
   return `gallery-container h-${randomColHeight}`;
 }
