0a462176bc4c18aa6b971a136ae34700bc897709
Aagam Vakil <aagamvakil@hotmail.com>
2022-03-18 16:13:46 -0400

    removed localhost 5001 in session_api_util and app.js file

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -6,17 +6,17 @@ export const setAuthToken = token => {
   } else {
     delete axios.defaults.headers.common['Authorization'];
   }
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
 };
 
 export const signup = (userData) => {
   debugger;
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
   return axios.post('/api/users/register', userData);
 };
 
 export const login = (userData) => {
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
   return axios.post('/api/users/login', userData);
 };
 
