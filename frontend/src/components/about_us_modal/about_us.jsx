4bc0fa43110a4e3f26459d5b1b8854c47597edb0
yakzaetz <yakzaetz@gmail.com>
2022-03-28 17:30:12 -0400

    a card I like

--- frontend/src/components/about_us_modal/about_us.jsx
+++ frontend/src/components/about_us_modal/about_us.jsx
@@ -1,50 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
 class AboutUs extends React.Component{
     render(){
-        return <div className="about-us-container">
-            <div className="about-me-card">
-                <div className="top-of-card">
-                    {/* <img src="" alt="" /> */}
+        return (
+        <div className="about-us-modal-container">
+            <div className="card_container">
+                <div className="au_modal_header">
+                    <p>About Us</p>
                 </div>
-                <div className="mid-of-card">
-                    <p>Aagam Vakil</p>
-                </div>
-                <div className="card-logos">
-                    <a href="https://www.linkedin.com/in/aagamvakil/"><FontAwesomeIcon icon={faLinkedin} id='company-icon' /><FontAwesomeIcon /></a>
-                    <a href="https://github.com/avakil3"><FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon /></a>
-                    <FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon />
-                </div>
-            </div>
-            <div className="about-me-card">
-                <div className="top-of-card">
-                    {/* <img src="" alt="" /> */}
-                </div>
-                <div className="mid-of-card">
-                    <p>Shawn Fries</p>
-                </div>
-                <div className="card-logos">
-                    <a href="https://www.linkedin.com/in/aagamvakil/"><FontAwesomeIcon icon={faLinkedin} id='company-icon' /><FontAwesomeIcon /></a>
-                    <a href="https://github.com/avakil3"><FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon /></a>
-                    <FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon />
-                </div>
-            </div>
-            <div className="about-me-card">
-                <div className="top-of-card">
-                    {/* <img src="" alt="" /> */}
-                </div>
-                <div className="mid-of-card">
-                    <p>Yaakov Zaetz</p>
-                </div>
-                <div className="card-logos">
-                    <a href="https://www.linkedin.com/in/aagamvakil/"><FontAwesomeIcon icon={faLinkedin} id='company-icon' /><FontAwesomeIcon /></a>
-                    <a href="https://github.com/avakil3"><FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon /></a>
-                    <FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon />
+                <div className="about-us-cards">
+                    <div className="about-me-card">
+                        <div className="top-of-card">
+                            {/* <img src="" alt="" /> */}
+                        </div>
+                        <div className="mid-of-card">
+                            <p>Aagam Vakil</p>
+                        </div>
+                        <div className="card-logos">
+                            <a href="https://www.linkedin.com/in/aagamvakil/"><FontAwesomeIcon icon={faLinkedin} id='company-icon' /><FontAwesomeIcon /></a>
+                            <a href="https://github.com/avakil3"><FontAwesomeIcon icon={faGithub} id='company-icon' /><FontAwesomeIcon /></a>
+                            <a href="https://app-academy.slack.com/archives/C036R7LLMS6/p1648236685016909"><FontAwesomeIcon icon={faCircleUser} id='company-icon' /><FontAwesomeIcon/></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        )
     }
 }
 
