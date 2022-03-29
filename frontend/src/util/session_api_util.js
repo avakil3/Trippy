ccb1ab4fd8d823129d2803ef948e5f41ab85223a
Aagam Vakil <aagamvakil@hotmail.com>
2022-03-28 21:07:31 -0400

    removed 5001 port

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -6,16 +6,16 @@ export const setAuthToken = token => {
   } else {
     delete axios.defaults.headers.common['Authorization'];
   }
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
 };
 
 export const signup = (userData) => {
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
   return axios.post('/api/users/register', userData);
 };
 
 export const login = (userData) => {
-  axios.defaults.baseURL = 'http://localhost:5001';
+  // axios.defaults.baseURL = 'http://localhost:5001';
   return axios.post('/api/users/login', userData);
 };
 
