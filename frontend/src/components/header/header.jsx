a878b1b5931573749cacb03959a7a7758a0efe0f
Aagam Vakil <aagamvakil@hotmail.com>
2024-01-05 11:28:31 -0500

    UI updates

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,69 +1,82 @@
-import React from 'react';
-import AboutUs from '../about_us_modal/about_us';
-import {Link} from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUserCircle, faAngleDown} from "@fortawesome/free-solid-svg-icons";
-import { withRouter } from '../../util/route_util';
-import { PortalWithState } from 'react-portal';
-import CreateBoardModalContainer from '../create_board_modal/create_board_modal_container';
+import React from "react";
+import AboutUs from "../about_us_modal/about_us";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { withRouter } from "../../util/route_util";
+import { PortalWithState } from "react-portal";
 
 class Header extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {click:true};
-        this.logout = this.logout.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { click: true };
+    this.logout = this.logout.bind(this);
+  }
 
   setClick() {
-      this.setState({click: !this.state.click})
+    this.setState({ click: !this.state.click });
   }
 
   logout(e) {
     e.preventDefault();
     this.props.logout();
-    this.props.router.navigate("/")
+    this.props.router.navigate("/");
   }
 
   render() {
     const handleClick = () => this.setClick();
     return (
-        <div className="header">
+      <div className="header">
         <div className="header-logo-container">
           <Link to="/home" className="header-link">
-            <img id="header-logo" src='images/trippy-logo-white.png' alt="logo" />
+            <img
+              id="header-logo"
+              src="images/trippy-logo-white.png"
+              alt="logo"
+            />
           </Link>
         </div>
-        <div className='header-two-items-on-right'>
+        <div className="header-two-items-on-right">
           <PortalWithState closeOnOutsideClick closeOnEsc>
             {({ openPortal, closePortal, isOpen, portal }) => (
               <React.Fragment>
-                <div className='about-us-btn'>
-                  <button id='about-us-btn' onClick={openPortal}><h4>About Us</h4></button>
+                <div className="about-us-btn">
+                  <button id="about-us-btn" onClick={openPortal}>
+                    <h4>About Us</h4>
+                  </button>
                 </div>
-                {portal(
-                  <AboutUs closeModal={closePortal} />
-                )}
+                {portal(<AboutUs closeModal={closePortal} />)}
               </React.Fragment>
             )}
           </PortalWithState>
-          
+
           <ul className="splash-nav-bar">
-            {/* <li className="links"><a className="links" href='https://github.com/avakil3/trippy' target="_blank" >GitHub</a></li> */}
-            {/* <li className="hidden">│</li> */}
             <li>
               <div className="header-dropdown" onClick={handleClick}>
-              <div className='header-user-info'>
+                <div className="header-user-info">
                   <FontAwesomeIcon icon={faUserCircle} id="avatar" />
                   <h4>{this.props.currentUser.username}</h4>
                   <FontAwesomeIcon icon={faAngleDown} id="dropdown" />
+                </div>
+                <ul
+                  className={this.state.click ? "dropdown clicked" : "dropdown"}
+                >
+                  <Link to={"/boards"} className="dropdown-option">
+                    My Boards
+                  </Link>
+                  <Link to={"/likes"} className="dropdown-option">
+                    My Likes
+                  </Link>
+                  <Link
+                    to={"/"}
+                    className="dropdown-option"
+                    onClick={this.logout}
+                  >
+                    Log out
+                  </Link>
+                </ul>
               </div>
-              <ul className={this.state.click ? "dropdown clicked" : "dropdown"}>
-                      <Link to={'/boards'} className="dropdown-option">My Boards</Link>
-                      <Link to={'/likes'} className="dropdown-option">My Likes</Link>
-                      <Link to={'/'} className="dropdown-option" onClick={this.logout}>Log out</Link>
-              </ul>
-              </div>
-          </li>
+            </li>
           </ul>
         </div>
       </div>
--- frontend/src/components/show_page/show_page.jsx
+++ frontend/src/components/show_page/show_page.jsx
@@ -1,120 +1,126 @@
-import React from 'react';
-import HeaderContainer from '../header/header_container';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faAngleDown,faLink } from '@fortawesome/free-solid-svg-icons'
-import Description from './description';
-import LikeContainer from '../like_item/Like_container';
-import { useNavigate } from 'react-router-dom';
-import DropdownContainer from './dropdown_container';
+import React from "react";
+import HeaderContainer from "../header/header_container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faLink } from "@fortawesome/free-solid-svg-icons";
+import LikeContainer from "../like_item/Like_container";
+import { useNavigate } from "react-router-dom";
+import DropdownContainer from "./dropdown_container";
 
-class ShowPage extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {clicked: false, liked: false, look: false}
-        this.toggleDropdown = this.toggleDropdown.bind(this);
-        this.toggleLike = this.toggleLike.bind(this);
-        this.toggleDescription = this.toggleDescription.bind(this); 
+class ShowPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false, liked: false, look: false };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleLike = this.toggleLike.bind(this);
+    this.toggleDescription = this.toggleDescription.bind(this);
+  }
+
+  toggleDropdown() {
+    this.setState({ clicked: !this.state.clicked });
+  }
+
+  toggleLike() {
+    this.setState({ liked: !this.state.liked });
+  }
+
+  toggleDescription() {
+    this.setState({ look: !this.state.look });
+  }
+
+  findPin(pins, pinId) {
+    for (let i = 0; i < pins.length; i++) {
+      const pin = pins[i];
+      if (pin._id === pinId) return pin;
     }
+  }
 
-    toggleDropdown(){
-        this.setState({clicked: !this.state.clicked})
-    }
+  componentDidMount() {
+    this.props.fetchPins();
+    this.props.fetchLikes(this.props.currentUser);
+    this.props
+      .fetchBoards(this.props.currentUser)
+      .then(() =>
+        this.props.boards.forEach((board) => this.props.fetchBoardPins(board))
+      );
+  }
 
+  capitalize(str) {
+    const letters = str.split("");
+    letters[0] = letters[0].toUpperCase();
+    return letters.join("");
+  }
 
-    toggleLike(){
-        this.setState({liked: !this.state.liked})
-    }
-
-
-    toggleDescription() {
-        this.setState({ look: !this.state.look })
-    }
-
-    findPin(pins, pinId){
-        for (let i = 0; i < pins.length; i++) {
-            const pin = pins[i];
-            if (pin._id === pinId) return pin
-        }
-    }
-
-
-    componentDidMount(){
-        this.props.fetchPins(); 
-        this.props.fetchLikes(this.props.currentUser);
-        this.props.fetchBoards(this.props.currentUser)
-            .then(()=> this.props.boards.forEach(board => this.props.fetchBoardPins(board)));
-    }
-
-    capitalize(str){
-        const letters = str.split("")
-        letters[0] = letters[0].toUpperCase();
-        return letters.join("")
-    }
-
-    render(){
-        const {pins} = this.props;
-        if (Object.values(pins).length === 0) return null; 
-        const pin = this.findPin(this.props.pins, this.props.router.params.pinId);
-        return(
-            <div className='whole-page'>
-                <div className='gradient show'></div>
-                <HeaderContainer/>
-                <div className='everything-but-the-header'>
-                    <BackButton />
-                    <div className='show-container-item-container'>
-                        <div className='show-container-item'>
-                            <div className='left-side-of-show-container'>
-                                <img src={pin.imageURL} alt={pin.title} />
-                            </div>
-                            <div className='right-side-of-show-container'>
-                                <div className='header-of-right-side-of-show'>
-                                 
-                                    <div className='header-button-container'>
-                                        {this.state.clicked ? < DropdownContainer pin={pin} /> : null}
-                                        <button onClick={this.toggleDropdown} >Save</button>
-                                    </div>
-                                </div>
-                                <div className='show-page-written-content'>
-                                    <div className='title-area-of-right-side-of-show'>
-                                        <a href={pin.extLink} target="_blank">{pin.title}<FontAwesomeIcon icon={faLink}/></a>
-                                        
-                                    </div>
-                                    <div className='region-location'>
-                                  
-                                        <div className='region-area-of-right-side-of-show'>
-                                            <p className="p-title">Location:</p>
-                                            <p className="p-body">{pin.location}</p>
-                                        </div>
-                                    </div>
-                                    <div className='description-area-of-right-side-of-show' onClick={this.toggleDescription} tabIndex="1">  
-                                            <p className="p-title" id='description-title'>Description</p>
-                                            <p id='description-angle'><FontAwesomeIcon icon={faAngleDown}/></p>
-                                        {this.state.look ? < Description description={pin.description}/> : null}
-                                    </div>
-
-                                        <div className='heart'> <LikeContainer pin={pin} /> </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='spacer'>
-                        {/* <FontAwesomeIcon icon={faArrowLeft} /> */}
-                    </div>
+  render() {
+    const { pins } = this.props;
+    if (Object.values(pins).length === 0) return null;
+    const pin = this.findPin(this.props.pins, this.props.router.params.pinId);
+    return (
+      <div className="whole-page">
+        <div className="gradient show"></div>
+        <HeaderContainer />
+        <div className="everything-but-the-header">
+          <BackButton />
+          <div className="show-container-item-container">
+            <div className="show-container-item">
+              <div className="left-side-of-show-container">
+                <img src={pin.imageURL} alt={pin.title} />
+              </div>
+              <div className="right-side-of-show-container">
+                <div className="header-of-right-side-of-show">
+                  <div className="header-button-container">
+                    {this.state.clicked ? (
+                      <DropdownContainer pin={pin} />
+                    ) : null}
+                    <button onClick={this.toggleDropdown}>Save</button>
+                  </div>
                 </div>
+                <div className="show-page-written-content">
+                  <div className="title-area-of-right-side-of-show">
+                    <a href={pin.extLink} target="_blank">
+                      {pin.title}
+                      <FontAwesomeIcon icon={faLink} />
+                    </a>
+                  </div>
+                  <div className="region-location">
+                    <div className="region-area-of-right-side-of-show">
+                      <p className="p-title">Location</p>
+                      <p className="p-body">{pin.location}</p>
+                    </div>
+                  </div>
+                  <div
+                    className="description-area-of-right-side-of-show"
+                    tabIndex="1"
+                  >
+                    <p className="p-title" id="description-title">
+                      Description
+                    </p>
+                    {/* <p id='description-angle'><FontAwesomeIcon icon={faAngleDown}/></p> */}
+                    {/* {this.state.look ? < Description description={pin.description}/> : null} */}
+                    <p>{pin.description}</p>
+                  </div>
+                </div>
+                <div className="heart">
+                  <LikeContainer pin={pin} />
+                </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+          <div className="spacer">
+            {/* <FontAwesomeIcon icon={faArrowLeft} /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ShowPage;
 
-
-
 function BackButton() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div onClick={() => navigate(-1)} id='left-arrow'><FontAwesomeIcon icon={faArrowLeft} /></div>
-  )
+    <div onClick={() => navigate(-1)} id="left-arrow">
+      <FontAwesomeIcon icon={faArrowLeft} />
+    </div>
+  );
 }
-
